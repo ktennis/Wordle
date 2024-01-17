4a54552187982992f16d3d9cf81aab81e858ebae
ipurnell <ianpurnell01@gmail.com>
2024-01-17 15:57:48 -0700

    check if real word + msg

--- Wordle.py
+++ Wordle.py
@@ -13,19 +13,34 @@ from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 def wordle():
 
     def enter_action(s):
-        gw.show_message("You have to implement this method.")
+        # initialize some variables
+        current_col = 0
+        userword = ""
+        
+        # get the user input
+        while current_col < N_COLS:
+            userword += gw.get_square_letter(gw.get_current_row(), current_col)
+            current_col += 1
+
+        userword = userword.lower()
+        print(userword)
+
+        # check if real word
+        if userword in FIVE_LETTER_WORDS:
+            gw.show_message("Completed Milestone 2!")
+        else:
+            pass
+        # insert msg if not real word
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
 
     newword = random.choice(FIVE_LETTER_WORDS)
 
-    current_col = N_COLS - N_COLS
-    current_row = N_ROWS - N_ROWS
-
-    while current_col < N_COLS:
-        gw.set_square_letter(current_row, current_col, newword[current_col])
-        current_col += 1
+    col = 0
+    while col < N_COLS:
+        gw.set_square_letter(gw.get_current_row(), col, newword[col])
+        col += 1
 
 # Startup code
 
